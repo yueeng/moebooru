77ea9f0b46cf5d356a5e169c21fb53895df17aa9
Rain Yue <ssiori@gmail.com>
2020-08-15 21:00:25 +0800

    up

--- app/src/main/java/com/github/yueeng/moebooru/MainActivity.kt
+++ app/src/main/java/com/github/yueeng/moebooru/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.github.yueeng.moebooru
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
+import androidx.paging.PagingData
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.github.yueeng.moebooru.databinding.ActivityMainBinding
+import com.github.yueeng.moebooru.databinding.ImageItemBinding
 
 class MainActivity : AppCompatActivity() {
+    private val adapter by lazy { ImageAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+        binding.recycler.adapter = adapter
+        Service.instance.post().observe(this) {
+            when (it) {
+                is ApiSuccessResponse -> {
+                    lifecycleScope.launchWhenCreated {
+                        adapter.submitData(PagingData.from(it.body))
+                    }
+                }
+            }
+        }
+    }
+
+    class ImageHolder(val binding: ImageItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    class ImageAdapter : PagingDataAdapter<JImageItem, ImageHolder>(diff) {
+        override fun onBindViewHolder(holder: ImageHolder, position: Int) {
+            holder.binding.image = getItem(position)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder =
+            ImageHolder(ImageItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+        companion object {
+            val diff = object : DiffUtil.ItemCallback<JImageItem>() {
+                override fun areItemsTheSame(oldItem: JImageItem, newItem: JImageItem): Boolean = oldItem.id == newItem.id
+                override fun areContentsTheSame(oldItem: JImageItem, newItem: JImageItem): Boolean = oldItem == newItem
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/github/yueeng/moebooru/Model.kt
@@ -0,0 +1,65 @@
+package com.github.yueeng.moebooru
+
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import kotlinx.android.parcel.Parcelize
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+@Parcelize
+data class JImageItem(
+    val actual_preview_height: Int,
+    val actual_preview_width: Int,
+    val author: String,
+    val change: Int,
+    val created_at: Int,
+    val creator_id: Int,
+    val file_size: Int,
+    val file_url: String,
+    val frames_pending_string: String,
+    val frames_string: String,
+    val has_children: Boolean,
+    val height: Int,
+    val id: Int,
+    val is_held: Boolean,
+    val is_shown_in_index: Boolean,
+    val jpeg_file_size: Int,
+    val jpeg_height: Int,
+    val jpeg_url: String,
+    val jpeg_width: Int,
+    val md5: String,
+    val preview_height: Int,
+    val preview_url: String,
+    val preview_width: Int,
+    val rating: String,
+    val sample_file_size: Int,
+    val sample_height: Int,
+    val sample_url: String,
+    val sample_width: Int,
+    val score: Int,
+    val source: String,
+    val status: String,
+    val tags: String,
+    val width: Int
+) : Parcelable
+
+interface MoebooruService {
+    @GET("post.json")
+    fun post(
+        @Query("page") page: Int = 1,
+        @Query("tags") tags: String = "rating:safe",
+        @Query("limit") limit: Int = 20
+    ): LiveData<ApiResponse<List<JImageItem>>>
+}
+
+object Service {
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addCallAdapterFactory(LiveDataCallAdapterFactory())
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okhttp)
+        .baseUrl("https://konachan.com")
+        .build()
+    val instance: MoebooruService = retrofit.create(MoebooruService::class.java)
+}
