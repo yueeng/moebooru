90f56e4554f57009d45e6b2ce725571c853ef151
Rain Yue <ssiori@gmail.com>
2020-09-05 16:34:07 +0800

    up

--- app/src/main/java/com/github/yueeng/moebooru/MainActivity.kt
+++ app/src/main/java/com/github/yueeng/moebooru/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -16,31 +17,35 @@ import androidx.paging.*
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.savedstate.SavedStateRegistryOwner
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.github.yueeng.moebooru.databinding.FragmentListBinding
 import com.github.yueeng.moebooru.databinding.FragmentMainBinding
 import com.github.yueeng.moebooru.databinding.ImageItemBinding
 import com.github.yueeng.moebooru.databinding.StateItemBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.filter
+import java.util.*
 
-class ImageDataSource : PagingSource<Int, JImageItem>() {
+class ImageDataSource(private val query: Q? = Q()) : PagingSource<Int, JImageItem>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, JImageItem> = try {
         val key = params.key ?: 1
-        val posts = Service.instance.post(page = key, Q().rating(Q.Rating.safe), limit = params.loadSize)
+        val posts = Service.instance.post(page = key, query!!.rating(Q.Rating.safe), limit = params.loadSize)
         LoadResult.Page(posts, null, if (posts.size == params.loadSize) key + 1 else null)
     } catch (e: Exception) {
         LoadResult.Error(e)
     }
 }
 
-class ImageViewModel(handle: SavedStateHandle) : ViewModel() {
-    val posts = Pager(PagingConfig(20)) { ImageDataSource() }.flow
+class ImageViewModel(handle: SavedStateHandle, defaultArgs: Bundle?) : ViewModel() {
+    val posts = Pager(PagingConfig(20)) { ImageDataSource(defaultArgs?.getParcelable("query")) }.flow
 }
 
-class ImageViewModelFactory(owner: SavedStateRegistryOwner, defaultArgs: Bundle?) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+class ImageViewModelFactory(owner: SavedStateRegistryOwner, private val defaultArgs: Bundle?) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T = ImageViewModel(handle) as T
+    override fun <T : ViewModel?> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T = ImageViewModel(handle, defaultArgs) as T
 }
 
 class MainActivity : AppCompatActivity() {
@@ -54,11 +59,35 @@ class MainActivity : AppCompatActivity() {
 }
 
 class MainFragment : Fragment() {
-    private val adapter by lazy { ImageAdapter() }
-    private val model: ImageViewModel by viewModels { ImageViewModelFactory(this, null) }
+    private val adapter by lazy { PagerAdapter(this) }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         FragmentMainBinding.inflate(inflater, container, false).also { binding ->
             (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+            binding.pager.adapter = adapter
+            TabLayoutMediator(binding.tab, binding.pager) { tab, position -> tab.text = adapter.data[position].first }.attach()
+        }.root
+
+    class PagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+        val data = listOf(
+            "Newest" to Q(),
+            "Day" to Q().popular_by_day(Date()),
+            "Week" to Q().popular_by_week(Date()),
+            "Month" to Q().popular_by_month(Date())
+        )
+
+        override fun getItemCount(): Int = data.size
+
+        override fun createFragment(position: Int): Fragment = ListFragment().apply {
+            arguments = bundleOf("query" to data[position].second, "name" to data[position].first)
+        }
+    }
+}
+
+class ListFragment : Fragment() {
+    private val adapter by lazy { ImageAdapter() }
+    private val model: ImageViewModel by viewModels { ImageViewModelFactory(this, arguments) }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        FragmentListBinding.inflate(inflater, container, false).also { binding ->
             binding.recycler.adapter = adapter.withLoadStateFooter(HeaderAdapter(adapter))
             lifecycleScope.launchWhenCreated {
                 adapter.loadStateFlow.collectLatest {
@@ -113,18 +142,31 @@ class MainFragment : Fragment() {
         fun bindTo(loadState: LoadState) {
             progressBar.isVisible = loadState is LoadState.Loading
             retry.isVisible = loadState is LoadState.Error
-            errorMsg.isVisible = !(loadState as? LoadState.Error)?.error?.message.isNullOrBlank()
-            errorMsg.text = (loadState as? LoadState.Error)?.error?.message
+            when (loadState) {
+                is LoadState.Error -> {
+                    errorMsg.isVisible = true
+                    errorMsg.text = loadState.error.message
+                }
+                is LoadState.NotLoading -> {
+                    errorMsg.isVisible = loadState.endOfPaginationReached
+                    errorMsg.text = if (loadState.endOfPaginationReached) "END" else null
+                }
+                else -> {
+                    errorMsg.isVisible = false
+                    errorMsg.text = null
+                }
+            }
         }
     }
 
     class HeaderAdapter(private val adapter: ImageAdapter) : LoadStateAdapter<HeaderHolder>() {
-        override fun onBindViewHolder(holder: HeaderHolder, loadState: LoadState) {
-            holder.bindTo(loadState)
+        override fun displayLoadStateAsItem(loadState: LoadState): Boolean = when (loadState) {
+            is LoadState.NotLoading -> loadState.endOfPaginationReached
+            else -> super.displayLoadStateAsItem(loadState)
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): HeaderHolder {
-            return HeaderHolder(parent) { adapter.retry() }
-        }
+        override fun onBindViewHolder(holder: HeaderHolder, loadState: LoadState) = holder.bindTo(loadState)
+
+        override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): HeaderHolder = HeaderHolder(parent) { adapter.retry() }
     }
 }
