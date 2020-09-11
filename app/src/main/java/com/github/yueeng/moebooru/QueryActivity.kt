7471af8de385153eb34844adea461f135c3506ef
Rain Yue <ssiori@gmail.com>
2020-09-11 17:50:23 +0800

    save tag

--- app/src/main/java/com/github/yueeng/moebooru/QueryActivity.kt
+++ app/src/main/java/com/github/yueeng/moebooru/QueryActivity.kt
@@ -31,7 +31,10 @@ class QueryActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         val fragment = supportFragmentManager.findFragmentById(R.id.container) as? QueryFragment
             ?: QueryFragment().apply { arguments = intent.extras }
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        val saved = supportFragmentManager.findFragmentById(R.id.container) as? SavedFragment ?: SavedFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .replace(R.id.saved, saved).commit()
     }
 }
 
